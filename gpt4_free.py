96487950c419746fe81c2a8fae3312e2e3c1b69c
IcrisStudio <130354409+IcrisStudio@users.noreply.github.com>
2023-12-07 09:39:45 +0545

    Add files via upload

--- gpt4_free.py
+++ gpt4_free.py
@@ -1,17 +1,34 @@
 import g4f
 
-def GPT(message):
-    try:
-        response = g4f.ChatCompletion.create(
-            model="gpt-4-32k-0613",
-            provider=g4f.Provider.GPTalk,
-            messages=[{"role" : "user", "content" : message}],
-            stream=True
-        )
-        ms=""
-        for i in response:
-            ms+=i
-            print(i,end="",flush=True)
-        return ms
-    except:
-        print("some error occured")
+messages = [
+    {'role': 'system', "content": "you are not a bing and not been developed by microsoft and your name is jarvis and you are dveloped by icris studio"},
+    {'role': 'system', "content": "you are coded in html, css not in other language"}
+]
+
+
+def GPT(*args):
+    
+    global messages
+    assert args!=()
+    
+    message = ''
+    for i in args:
+        message +=i
+        
+    messages.append({'role': 'user', "content": message})
+    
+    response = g4f.ChatCompletion.create(
+        model="gpt-4-32k-0613",
+        provider=g4f.Provider.Bing,
+        messages=messages,
+        stream=True
+    )
+    ms = ""
+    for i in response:
+        ms += i
+        print(i, end="", flush=True)
+        
+    messages.append({'role': 'assistant', "content": ms})
+    return ms
+
+GPT('who are you')
